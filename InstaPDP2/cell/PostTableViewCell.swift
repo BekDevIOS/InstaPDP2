259998336ed4bc8bef7e01bf8b8dbb0eae0eab01
BekDevIOS <umekhmonboev@gmail.com>
2023-10-06 21:26:47 +0900

    updeted project!

---
+++ InstaPDP2/cell/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  InstaPDP2
+//
+//  Created by Utkirbek Mekhmonboev on 10/6/23.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ InstaPDP2/model/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  InstaPDP2
+//
+//  Created by Utkirbek Mekhmonboev on 10/6/23.
+//
+
+import Foundation
